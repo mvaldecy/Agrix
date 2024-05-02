2abe890dd8f8aae47b8f4b28a08f71865b700ff3
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-05-02 18:03:01 -0300

    feat: PersonService tests

---
+++ src/test/java/com/betrybe/agrix/solution/PersonServiceTest.java
@@ -0,0 +1,82 @@
+package com.betrybe.agrix.solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import com.betrybe.agrix.ebytr.staff.exception.PersonNotFoundException;
+import com.betrybe.agrix.ebytr.staff.repository.PersonRepository;
+import com.betrybe.agrix.ebytr.staff.security.Role;
+import com.betrybe.agrix.ebytr.staff.service.PersonService;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+
+/**
+ * PerService test.
+ */
+@SpringBootTest
+@ActiveProfiles("test")
+public class PersonServiceTest {
+    
+  @Autowired
+  PersonService personService;
+    
+  @MockBean
+  PersonRepository personRepository;
+
+  @Test
+  public void getPersonByIdExistsTest() {
+    Person personMock = new Person();
+    Role roleMock = Role.ADMIN;
+    personMock.setUsername("Marcos Valdecy");
+    personMock.setId(23L);
+    personMock.setPassword("senha");
+    personMock.setRole(roleMock);
+
+    Mockito.when(personRepository.findById(eq(23L)))
+        .thenReturn(Optional.of(personMock));
+    
+    Person returnedPerson = personService.getPersonById(23L);
+    assertEquals(returnedPerson.getUsername(), personMock.getUsername());
+    assertEquals(returnedPerson.getId(), personMock.getId());
+    assertEquals(returnedPerson.getPassword(), personMock.getPassword());
+    assertEquals(returnedPerson.getRole(), personMock.getRole());
+  }
+
+  @Test
+  public void getPersonByIdNotFound() {
+    Mockito.when(personRepository.findById(any()))
+        .thenReturn(Optional.empty());
+    
+    assertThrows(PersonNotFoundException.class, () -> personService.getPersonById(5L));
+
+    Mockito.verify(personRepository).findById(5L);
+  }
+
+  @Test
+  public void getPersonByUsernameExists() {
+    Person personMock = new Person();
+    Role roleMock = Role.ADMIN;
+    personMock.setUsername("Marcos Valdecy");
+    personMock.setId(23L);
+    personMock.setPassword("senha");
+    personMock.setRole(roleMock);
+
+    Mockito.when(personRepository.findByUsername("Marcos Valdecy"))
+        .thenReturn(Optional.of(personMock));
+    
+    Person returnedPerson = personService.getPersonByUsername("Marcos Valdecy");
+    assertEquals(returnedPerson.getUsername(), personMock.getUsername());
+    assertEquals(returnedPerson.getId(), personMock.getId());
+    assertEquals(returnedPerson.getPassword(), personMock.getPassword());
+    assertEquals(returnedPerson.getRole(), personMock.getRole());
+  }
+}
