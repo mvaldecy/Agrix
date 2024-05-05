5d8156bd05a6770f83a7af82018247b960d63d82
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-05-05 01:36:33 -0300

    feat: fertilizer not found exception

---
+++ src/main/java/com/betrybe/agrix/service/exception/FertilizerNotFoundException.java
@@ -0,0 +1,14 @@
+package com.betrybe.agrix.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * FertilizerNotFoundException.
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public abstract class FertilizerNotFoundException extends NotFoundException {
+  public FertilizerNotFoundException() {
+    super("Fertilizante não encontrado!");
+  }
+}
